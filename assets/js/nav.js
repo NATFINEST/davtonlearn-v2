4afbaddee74edb3384fae2f53f2c3e36b3a88646
NATFINEST <ese.nat4@gmail.com>
2018-09-22 00:12:22 +0100

    Official

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -29,20 +29,26 @@ $('.single-page-nav').singlePageNav({
 });
 });
 
-        $(document).on('click', 'div .dropdown-menu', function (e) {
-        e.stopPropagation();
-      });
-      $('.request').click(function() {
-        $(this).parents('.dropup').find('a.dropdown-toggle').dropdown('toggle')
-      });
-      $("#hide").click(function(){
-          $(".ad").hide();
-      });
-      $("#signup-link").click(function(){
-        $('#login').modal('hide');
-        $('#signup').modal('show');
-      });
-      $("#login-link").click(function(){
-        $('#login').modal('show');
-        $('#signup').modal('hide');
-      });
+    $(document).on('click', 'div .dropdown-menu', function (e) {
+    e.stopPropagation();
+  });
+  $('.request').click(function() {
+    $(this).parents('.dropup').find('a.dropdown-toggle').dropdown('toggle')
+  });
+  $("#hide").click(function(){
+      $(".ad").hide();
+  });
+  $("#signup-link").click(function(){
+    $('#login').modal('hide');
+    $('#signup').modal('show');
+  });
+  $("#login-link").click(function(){
+    $('#login').modal('show');
+    $('#signup').modal('hide');
+  });
+
+  $(document).ready(function(){
+   $('[data-toggle="offcanvas"]').click(function(){
+       $("#navigation").toggleClass("hidden-xs");
+   });
+});
